94181ea3eb9f38e8b7a400310699bec01d1b8791
gech11 <getachewfikadu77@gmail.com>
2021-04-19 16:56:17 +0300

    initial commit

--- resources/views/video/watch.blade.php
+++ resources/views/video/watch.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 @section('content')
     <div>
-        
+        <video src=""></video>
+        <div>
+            video controll
+        </div>
+        <div>
+            title like share
+        </div>
+        <div>
+            channel suscribe
+        </div>
+        <div>
+            comments
+        </div>
+        <div>
+            recomended
+        </div>
     </div>
 @endsection

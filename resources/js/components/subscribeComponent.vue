6b9e4c3853e793f2edffb596e31642530e2cc206
getachew fikadu <getachewfikadu77@gmail.com>
2021-04-20 14:55:38 +0300

    initial commit

--- resources/js/components/subscribeComponent.vue
+++ resources/js/components/subscribeComponent.vue
@@ -1,11 +1,25 @@
 <template>
-<button @click="suscribe"></button>
+<button @click="suscribe">{{button}}</button>
 </template>
 <script>
 export default {
+    props:['userId','channelId'],
     data(){
         return{
-
+          button:'subscribe',
+          subscribed:false
+        }
+    },
+    mounted(){
+      axios.get('api/')
+    },
+    methods:{
+        subscribe(){
+            axios.post('/api/suscribe',{this.userId,this.channelId})
+            .then(res=>{
+             this.buttonText='un'.this.buttonText;
+             this.subscribed=!this.subscribed;
+            })
         }
     }
 

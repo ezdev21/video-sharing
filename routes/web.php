dd41825c695b61c0c259b960b03bbd080f3087f8
gech11 <getachewfikadu77@gmail.com>
2021-05-03 14:23:10 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/',[VideoController::class,'index']);
+
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 

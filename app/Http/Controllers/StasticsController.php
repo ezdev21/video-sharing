748be83abd6de3a000a42bd8408c094ae0f28ebc
gech11 <getachewfikadu77@gmail.com>
2021-12-27 16:55:39 -0500

    stastics feature added

---
+++ app/Http/Controllers/StasticsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStasticsRequest;
+use App\Http\Requests\UpdateStasticsRequest;
+use App\Models\Stastics;
+
+class StasticsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStasticsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStasticsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Stastics  $stastics
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stastics $stastics)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Stastics  $stastics
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stastics $stastics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStasticsRequest  $request
+     * @param  \App\Models\Stastics  $stastics
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStasticsRequest $request, Stastics $stastics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Stastics  $stastics
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stastics $stastics)
+    {
+        //
+    }
+}

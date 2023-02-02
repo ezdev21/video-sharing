022a9973a857a23b90ee282d0d80e20807a78809
ezra02 <ezrafikadu02@gmail.com>
2023-02-02 21:18:55 +0300

    social logins using socialite added

--- config/services.php
+++ config/services.php
@@ -29,5 +29,22 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@ use App\Http\Controllers\ChannelController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\StasticsController;
 use App\Http\Controllers\VideoController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/',[VideoController::class,'index'])->name('video.index');
 
@@ -55,4 +57,69 @@ Route::prefix('comment')->group(function(){
 Route::prefix('user')->group(function(){
   Route::post('notifications',[UserController::class,'notifications']);
   Route::patch('notification/mark-as-read',[UserController::class,'readNotification']);
+});
+
+//socialite login routes
+
+Route::get('/auth-provider/google/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth-provider/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+
+    $user = User::updateOrCreate([
+        'google_id' => $googleUser->id,
+    ], [
+        'name' => $googleUser->name,
+        'email' => $googleUser->email,
+        'google_token' => $googleUser->token,
+        'google_refresh_token' => $googleUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/');
+});
+
+Route::get('/auth-provider/facebook/redirect', function () {
+    return Socialite::driver('facebook')->redirect();
+});
+
+Route::get('/auth-provider/facebook/callback', function () {
+    $facebookUser = Socialite::driver('facebook')->user();
+
+    $user = User::updateOrCreate([
+        'facebook_id' => $facebookUser->id,
+    ], [
+        'name' => $facebookUser->name,
+        'email' => $facebookUser->email,
+        'facebook_token' => $facebookUser->token,
+        'facebook_refresh_token' => $facebookUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/');
+});
+
+Route::get('/auth-provider/twitter/redirect', function () {
+    return Socialite::driver('twitter')->redirect();
+});
+
+Route::get('/auth-provider/twitter/callback', function () {
+    $twitterUser = Socialite::driver('twitter')->user();
+
+    $user = User::updateOrCreate([
+        'twitter_id' => $twitterUser->id,
+    ], [
+        'name' => $twitterUser->name,
+        'email' => $twitterUser->email,
+        'twitter_token' => $twitterUser->token,
+        'twitter_refresh_token' => $twitterUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/');
 });
